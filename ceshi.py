82e136619d082f02a408ac34397059ef5be362fa
flyone <flystary@github.com>
2020-09-27 14:22:34 +0800

    commint

--- ceshi.py
+++ ceshi.py
@@ -25,3 +25,36 @@ x = 10
 print(type(x) #获取数据类型
 )
 """
+"""
+import random
+print(random.randrange(1,20))
+"""
+"""
+a='''Python is a widely used general-purpose, high level programming language. 
+It was initially designed by Guido van Rossum in 1991 
+and developed by Python Software Foundation. 
+It was mainly developed for emphasis on code readability, 
+and its syntax allows programmers to express concepts in fewer lines of code.'''
+print(a)
+"""
+#b="hello,world!"
+#print(b[0:12])
+#print(b[-6:-1])
+#print(len(b))
+
+"""
+a="  Hello,World!  "
+print(a)
+print(a.strip())
+print(a.lower())
+print(a.upper())
+print(a.replace("World","liql"))
+print(a.split(","))
+"""
+txt = "China is a great country"
+#x= "eat" in txt
+x = "aet" not in txt
+print(x)
+#print(txt.split(" "))
+#print(txt.split("s"))
+
